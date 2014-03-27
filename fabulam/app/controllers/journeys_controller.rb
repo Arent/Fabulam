39a0a61c4aa3053c69d5f4809332c0fa5ffb7db7
Arent <arent.stienstra@gmail.com>
2014-03-26 22:41:19 -0400

    Fotoswerken

--- fabulam/app/controllers/journeys_controller.rb
+++ fabulam/app/controllers/journeys_controller.rb
@@ -1,11 +1,33 @@
 class JourneysController < ApplicationController
+require 'fileutils'
+
  def index
  @current_user ||= User.find(session[:user_id]) if session[:user_id]
   @journey = Journey.where(uid:   current_user.uid)
  end
  
  def show
+ @current_user ||= User.find(session[:user_id]) if session[:user_id]
   @journey = Journey.find(params[:id])
+  naam = "/uploads/"+current_user.uid+"/"+params[:id]+"/1.jpg"
+   if FileTest.exists?(Rails.root.to_s+'/public'+naam)
+        p "bestand bestaat"
+        @photolink = naam
+   else
+        p "bestand bestaat niet"
+        @photolink =""
+   end
+   
+     naam2 = "/uploads/"+current_user.uid+"/"+params[:id]+"/2.jpg"
+   if FileTest.exists?(Rails.root.to_s+'/public'+naam)
+        p "bestand bestaat"
+        @photolink2 = naam2
+   else
+        p "bestand bestaat niet"
+        @photolink2 =""
+   end
+   
+   
  end
 
  def new
@@ -22,10 +44,27 @@ class JourneysController < ApplicationController
   else
     render 'new'
   end
+  directory_name = Rails.root.join('public', 'uploads', current_user.uid, Journey.last.id.to_s)
+   Dir.mkdir(directory_name) unless File.exists?(directory_name)
+   
+  uploaded_io = params[:journey][:image]
+  File.open(Rails.root.join('public', 'uploads', current_user.uid, Journey.last.id.to_s,'1'+File.extname(uploaded_io.original_filename).to_s), 'wb') do |file|
+    file.write(uploaded_io.read)
+  end
+    
+      uploaded_io = params[:journey][:image2]
+  File.open(Rails.root.join('public', 'uploads', current_user.uid, Journey.last.id.to_s,'2'+File.extname(uploaded_io.original_filename).to_s), 'wb') do |file|
+    file.write(uploaded_io.read)
+  end
+  
+  
+  
  end
   
  def edit
  @journey = Journey.find(params[:id])
+
+ 
  end
   
  def update
@@ -39,6 +78,14 @@ class JourneysController < ApplicationController
  end
 
  def destroy
+     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    naam = Rails.root.to_s+"/public/uploads/"+current_user.uid+"/"+params[:id]
+    p naam
+    @journey = Journey.find(params[:id])
+    @journey.destroy
+    FileUtils.remove_dir naam, true
+
+    redirect_to journeys_path
  end
 
 end
--- fabulam/app/models/journey.rb
+++ fabulam/app/models/journey.rb
@@ -1,7 +1,6 @@
 class Journey < ActiveRecord::Base
 belongs_to :user
 validates :uid, presence: true
-mount_uploader :image, ImageUploader
 
 
 end
