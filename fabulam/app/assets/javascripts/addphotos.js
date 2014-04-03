888bd5f8f911d6d6a1103fb14151d36023fa66a1
Arent <arent.stienstra@gmail.com>
2014-04-03 13:10:19 -0400

    css gestyled

--- fabulam/app/assets/javascripts/addphotos.js
+++ fabulam/app/assets/javascripts/addphotos.js
@@ -3,10 +3,16 @@
     window.addfile = function() {
         var input = document.createElement('input');
          input.id   = 'journey_image' + i ;
-         input.name = 'journey[image' + i++ + ']'; 
+         input.name = 'journey[image' + i + ']'; 
          input.type = 'file';
        
 
         document.getElementById('form_name').appendChild(input);
+        var text = document.createElement('input');
+         text.id   = 'journey_text' + i ;
+         text.name = 'journey[text' + i++ + ']'; 
+         text.type = 'area';
+         document.getElementById('form_name').appendChild(text);
+        
     }
 })();
